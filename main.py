57727149da720ce141c1613c5cf376b2a6ee08f4
Bibhuti29 <bibhutinayak309@gmail.com>
2025-08-26 01:13:44 +0530

    2nd

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from pytubefix import YouTube
-from pytubefix.cli import on_progress 
+from pytubefix.cli import on_progress
+import os
 
 app = FastAPI()
+
+app.mount("/videos", StaticFiles(directory="Download"), name="videos")
 
 
 @app.get("/")
@@ -15,4 +19,11 @@ def get_video(url: str):
     url = f'https://www.youtube.com/watch?v={url}'
     yt = YouTube(url, on_progress_callback=on_progress)
     stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-    stream.download('./Download') 
+    os.makedirs("Download", exist_ok=True)
+    filepath = stream.download("Download")
+    
+    filename = os.path.basename(filepath)
+     
+    video_url = f"/videos/{filename}"
+    
+    return {"video_url": video_url}
